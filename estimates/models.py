75c0923e2efeb4ebf73b528ab0697135ff33071b
tiwamoto <t.ndc.000001@outlook.jp>
2024-02-21 12:48:45 +0900

    登録・更新・削除まで完了

--- estimates/models.py
+++ estimates/models.py
@@ -31,97 +31,134 @@ class Estimate(models.Model):
         verbose_name="案件名"
     )
 
+    # 見積書
+    rouph_estimate_files = models.FileField(
+        upload_to=estimate_upload_to,
+        blank=True,
+        null=True,
+        verbose_name="概算見積書"
+    )
+
     # 見積時(予測)
     rouph_estimate_man_hour = models.DecimalField(
         max_digits=5, 
         decimal_places=2,
+        blank=True,
+        null=True,
         verbose_name="工数"
     )
 
     rouph_estimate_unit = models.CharField(
         verbose_name="",
         max_length=20, 
+        blank=True,
+        null=True,
         choices=UNIT_CHOICES
     )
 
     rouph_estimate_delivary_date = models.DateField(
         default=timezone.now,
+        blank=True,
+        null=True,
         verbose_name="納期"
+    )
+
+    quotation_deadline = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="見積提示時期"
     )
 
     is_stoped = models.BooleanField(
         default=False,
+        null=True,
         verbose_name="中止"        
     )
 
     # 見積書
     estimate_files = models.FileField(
         upload_to=estimate_upload_to,
+        blank=True,
+        null=True,
         verbose_name="見積書"
-    )
-
-    estimate_type = models.CharField(
-        max_length=20, 
-        choices=ESTIMATE_TYPE,
-        verbose_name="概算・見積"
     )
 
     estimate_man_hour = models.DecimalField(
         max_digits=5, 
         decimal_places=2,
+        blank=True,
+        null=True,
         verbose_name="工数"        
     )
 
     estimate_unit = models.CharField(
         verbose_name="",
         max_length=20, 
+        blank=True,
+        null=True,
         choices=UNIT_CHOICES
     )
 
     estimate_delivary_date = models.DateField(
         default=timezone.now,
+        blank=True,
+        null=True,
         verbose_name="納期"
     )
 
     go_live_date = models.DateField(
+        blank=True,
+        null=True,
         verbose_name="本稼働日"
     )
 
     received_date = models.DateField(
+        blank=True,
+        null=True,
         verbose_name="受注日"
     )
 
-    expected_delivery_date = models.DateField(
-        verbose_name="納品予定日"
-    )
-
     expected_inspection_date = models.DateField(
+        blank=True,
+        null=True,
         verbose_name="検収予定日"
     )
 
     quotation = models.FileField(
         upload_to=placing_upload_to,
+        blank=True,
+        null=True,
         verbose_name="見積書"
     )
 
     delivery_date = models.DateField(
+        blank=True,
+        null=True,
         verbose_name="納品日"
     )
 
     delevery_receipt = models.FileField(
         upload_to=placing_upload_to,
+        blank=True,
+        null=True,
         verbose_name="納品書"
     )
 
     inspection_date = models.DateField(
+        blank=True,
+        null=True,
         verbose_name="検収日"
     )
 
     inspection_receipt = models.FileField(
         upload_to=placing_upload_to,
+        blank=True,
+        null=True,
         verbose_name="検収書"
     )
 
     description = models.TextField(
+        blank=True,
+        null=True,
         verbose_name="備考"
     )
--- estimates/views.py
+++ estimates/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SingleEstimateMOdelForm
 from .models import Estimate
 
@@ -6,7 +6,12 @@ from .models import Estimate
 # Create your views here.
 def index(request,url=None):
     if request.method == 'GET':
-        context = {}
+        estimates = Estimate.objects.filter(inspection_date__isnull=True)
+
+        context = {
+            'message' : "",
+            'estimates' : estimates
+        }
         return render(request, 'estimate/index.html', context)
 
 def input(request,url=None):
@@ -14,6 +19,43 @@ def input(request,url=None):
         # singleEstimateMOdelForm = SingleEstimateMOdelForm()
         form = SingleEstimateMOdelForm()
         context = {
+            'message' : "",
             'form' : form
         }
         return render(request, 'estimate/input.html', context)
+    else:
+        form = SingleEstimateMOdelForm(request.POST)
+
+        if form.is_valid():
+            estimate = Estimate()
+
+            estimate.rouph_estimate_name = form.cleaned_data['rouph_estimate_name']
+            estimate.save()
+            return redirect('/estimate/')
+        
+def modify(request,id,url=None):
+    if request.method == 'GET':
+        if id:
+            estimate_result = Estimate.objects.get(pk=id)
+            form = SingleEstimateMOdelForm(instance=estimate_result)
+        context = {
+            'message' : "",
+            'form' : form
+        }
+        return render(request, 'estimate/input.html', context)
+    
+    else:
+        form = SingleEstimateMOdelForm(request.POST)
+
+        if form.is_valid():
+            estimate = Estimate.objects.get(pk=id)
+
+            estimate.rouph_estimate_name = form.cleaned_data['rouph_estimate_name']
+            estimate.save()
+            return redirect('/estimate/')
+
+def delete(request,id,url=None):
+    if request.method == 'GET':
+        estimate = Estimate.objects.get(pk=id)
+        estimate.delete()
+        return redirect('/estimate/')
